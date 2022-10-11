584096bdad3e77a4e7f21cb1ca195d8ca42df9ab
wonderpops <supstump@gmail.com>
2022-10-11 18:44:46 +1000

    Add pagination to articles preview list

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -16,21 +16,19 @@ class HomeScreenWidget extends StatefulWidget {
 
 class _HomeScreenWidgetState extends State<HomeScreenWidget> {
   late ArticlesBloc articlesBloc;
-  // late ScrollController scrollController;
+  late PageController pageController;
 
   @override
   void initState() {
     articlesBloc = BlocProvider.of<ArticlesBloc>(context);
     articlesBloc.add(ArticlesLoadEvent());
-    // scrollController = ScrollController()..addListener(loadMoreArticles);
+    pageController = PageController();
     super.initState();
   }
 
-  // loadMoreArticles() async {
-  //   if (scrollController.offset == scrollController.position.maxScrollExtent) {
-
-  //   }
-  // }
+  jumpToPage(pageIndex) {
+    pageController.jumpToPage(pageIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,103 +48,170 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
                 ArticlesLoadedState articlesLoadedState =
                     articlesBloc.state as ArticlesLoadedState;
                 List<Article> articles = articlesLoadedState.articles;
-                return ListView.builder(
-                  // controller: scrollController,
-                  itemCount: articles.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 150,
-                        child: Card(
-                          clipBehavior: Clip.hardEdge,
-                          elevation: 10,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              AspectRatio(
-                                aspectRatio: 1,
-                                child: FittedBox(
-                                  fit: BoxFit.cover,
-                                  clipBehavior: Clip.hardEdge,
-                                  child: Image.network(
-                                      articles[index].multimedia.first.url),
-                                ),
-                              ),
-                              Flexible(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16, vertical: 8),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      AutoSizeText(
-                                        articles[index].title,
-                                        style: GoogleFonts.barlowCondensed(),
-                                        minFontSize: 18,
-                                        maxLines: 5,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      Flexible(
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: [
-                                            Flexible(
-                                              child: Row(
-                                                children: [
-                                                  const Icon(Icons.person,
-                                                      size: 16),
-                                                  Flexible(
-                                                    child: AutoSizeText(
-                                                      articles[index].byline,
-                                                      maxFontSize: 12,
-                                                      maxLines: 1,
-                                                      overflow:
-                                                          TextOverflow.ellipsis,
-                                                    ),
-                                                  )
-                                                ],
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Flexible(
-                                        child: Row(
-                                          children: [
-                                            const Icon(Icons.date_range,
-                                                size: 16),
-                                            Flexible(
-                                              child: AutoSizeText(
-                                                DateFormat('yyyy.MM.dd kk:mm')
-                                                    .format(DateTime.parse(
-                                                        articles[index]
-                                                            .createdDate)),
-                                                maxFontSize: 12,
-                                                maxLines: 1,
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
+                int maxArticlesOnPageCount = 5;
+                return PageView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    controller: pageController,
+                    itemCount:
+                        (articles.length / maxArticlesOnPageCount).ceil(),
+                    itemBuilder: (context, page) {
+                      List<Article> pageArticles;
+                      if ((page + 1) * maxArticlesOnPageCount <
+                          articles.length) {
+                        pageArticles = articles
+                            .getRange(
+                              page * maxArticlesOnPageCount,
+                              (page + 1) * maxArticlesOnPageCount,
+                            )
+                            .toList();
+                      } else {
+                        pageArticles = articles
+                            .skip(page * maxArticlesOnPageCount)
+                            .toList();
+                      }
+
+                      return ListView(
+                        children: pageArticles
+                                .map<Widget>((article) =>
+                                    _ArticlePreviewWidget(article: article))
+                                .toList() +
+                            [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Opacity(
+                                      opacity: page == 0 ? 0 : 1,
+                                      child: TextButton(
+                                          onPressed: () {
+                                            if (page != 0) {
+                                              jumpToPage(page - 1);
+                                            }
+                                          },
+                                          child: const Text('Previous page')),
+                                    ),
+                                    Text(
+                                        '${page + 1} of ${(articles.length / maxArticlesOnPageCount).ceil()}'),
+                                    Opacity(
+                                      opacity: page ==
+                                              (articles.length /
+                                                          maxArticlesOnPageCount)
+                                                      .ceil() -
+                                                  1
+                                          ? 0
+                                          : 1,
+                                      child: TextButton(
+                                          onPressed: () {
+                                            if (page !=
+                                                (articles.length /
+                                                            maxArticlesOnPageCount)
+                                                        .ceil() -
+                                                    1) {
+                                              jumpToPage(page + 1);
+                                            }
+                                          },
+                                          child: const Text('Next page')),
+                                    ),
+                                  ],
                                 ),
                               )
                             ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                );
+                      );
+                    });
               default:
                 return Container();
             }
           },
+        ),
+      ),
+    );
+  }
+}
+
+class _ArticlePreviewWidget extends StatelessWidget {
+  const _ArticlePreviewWidget({super.key, required this.article});
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 150,
+        child: Card(
+          clipBehavior: Clip.hardEdge,
+          elevation: 10,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: FittedBox(
+                  fit: BoxFit.cover,
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.network(article.multimedia.first.url),
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AutoSizeText(
+                        article.title,
+                        style: GoogleFonts.barlowCondensed(),
+                        minFontSize: 18,
+                        maxLines: 5,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Flexible(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Flexible(
+                              child: Row(
+                                children: [
+                                  const Icon(Icons.person, size: 16),
+                                  Flexible(
+                                    child: AutoSizeText(
+                                      article.byline,
+                                      maxFontSize: 12,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Flexible(
+                        child: Row(
+                          children: [
+                            const Icon(Icons.date_range, size: 16),
+                            Flexible(
+                              child: AutoSizeText(
+                                DateFormat('yyyy.MM.dd kk:mm').format(
+                                    DateTime.parse(article.createdDate)),
+                                maxFontSize: 12,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
