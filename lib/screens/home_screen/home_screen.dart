d18c8a46da41e91bd5cbf4fd5f3e3f65e3f5d47a
wonderpops <supstump@gmail.com>
2022-10-12 16:39:47 +1000

    Rework home screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -30,101 +30,100 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
     pageController.jumpToPage(pageIndex);
   }
 
+  List<Widget> buildArticlesPage(
+      context, List<Article> pageArticles, int currentPage, int pageCount) {
+    List<Widget> pageWidgets = [];
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    pageWidgets.add(
+      AppBar(
+        title: const Text('The NYT top stories'),
+        primary: false,
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+      ),
+    );
+
+    pageWidgets += pageArticles
+        .map<Widget>((article) => _ArticlePreviewWidget(article: article))
+        .toList();
+
+    pageWidgets.add(pageChangerWidget(currentPage, pageCount));
+    return pageWidgets;
+  }
+
+  Widget pageChangerWidget(int currentPage, int pageCount) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Opacity(
+            opacity: currentPage == 0 ? 0 : 1,
+            child: TextButton(
+                onPressed: () {
+                  if (currentPage != 0) {
+                    jumpToPage(currentPage - 1);
+                  }
+                },
+                child: const Text('Previous page')),
+          ),
+          Text('${currentPage + 1} of $pageCount'),
+          Opacity(
+            opacity: currentPage == pageCount - 1 ? 0 : 1,
+            child: TextButton(
+                onPressed: () {
+                  if (currentPage != pageCount) {
+                    jumpToPage(currentPage + 1);
+                  }
+                },
+                child: const Text('Next page')),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('NYT Top Stories'),
-          backgroundColor: colorScheme.primaryContainer,
-          automaticallyImplyLeading: false,
-        ),
-        extendBody: true,
-        body: BlocBuilder<ArticlesBloc, ArticlesState>(
-          builder: (context, state) {
-            switch (state.runtimeType) {
-              case ArticlesLoadedState:
-                ArticlesLoadedState articlesLoadedState =
-                    articlesBloc.state as ArticlesLoadedState;
-                List<Article> articles = articlesLoadedState.articles;
-                int maxArticlesOnPageCount = 5;
-                return PageView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    controller: pageController,
-                    itemCount:
-                        (articles.length / maxArticlesOnPageCount).ceil(),
-                    itemBuilder: (context, page) {
-                      List<Article> pageArticles;
-                      if ((page + 1) * maxArticlesOnPageCount <
-                          articles.length) {
-                        pageArticles = articles
-                            .getRange(
-                              page * maxArticlesOnPageCount,
-                              (page + 1) * maxArticlesOnPageCount,
-                            )
-                            .toList();
-                      } else {
-                        pageArticles = articles
-                            .skip(page * maxArticlesOnPageCount)
-                            .toList();
-                      }
+    return Scaffold(
+      extendBody: true,
+      backgroundColor: colorScheme.surfaceVariant.withOpacity(.4),
+      body: BlocBuilder<ArticlesBloc, ArticlesState>(
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case ArticlesLoadedState:
+              ArticlesLoadedState articlesLoadedState =
+                  articlesBloc.state as ArticlesLoadedState;
+              List<Article> articles = articlesLoadedState.articles;
+              int maxArticlesOnPageCount = 5;
+              return PageView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: pageController,
+                  itemCount: (articles.length / maxArticlesOnPageCount).ceil(),
+                  itemBuilder: (context, page) {
+                    List<Article> pageArticles;
+                    if ((page + 1) * maxArticlesOnPageCount < articles.length) {
+                      pageArticles = articles
+                          .getRange(
+                            page * maxArticlesOnPageCount,
+                            (page + 1) * maxArticlesOnPageCount,
+                          )
+                          .toList();
+                    } else {
+                      pageArticles =
+                          articles.skip(page * maxArticlesOnPageCount).toList();
+                    }
 
-                      return ListView(
-                        children: pageArticles
-                                .map<Widget>((article) =>
-                                    _ArticlePreviewWidget(article: article))
-                                .toList() +
-                            [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Opacity(
-                                      opacity: page == 0 ? 0 : 1,
-                                      child: TextButton(
-                                          onPressed: () {
-                                            if (page != 0) {
-                                              jumpToPage(page - 1);
-                                            }
-                                          },
-                                          child: const Text('Previous page')),
-                                    ),
-                                    Text(
-                                        '${page + 1} of ${(articles.length / maxArticlesOnPageCount).ceil()}'),
-                                    Opacity(
-                                      opacity: page ==
-                                              (articles.length /
-                                                          maxArticlesOnPageCount)
-                                                      .ceil() -
-                                                  1
-                                          ? 0
-                                          : 1,
-                                      child: TextButton(
-                                          onPressed: () {
-                                            if (page !=
-                                                (articles.length /
-                                                            maxArticlesOnPageCount)
-                                                        .ceil() -
-                                                    1) {
-                                              jumpToPage(page + 1);
-                                            }
-                                          },
-                                          child: const Text('Next page')),
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
-                      );
-                    });
-              default:
-                return Container();
-            }
-          },
-        ),
+                    return ListView(
+                        children: buildArticlesPage(context, pageArticles, page,
+                            (articles.length / maxArticlesOnPageCount).ceil()));
+                  });
+            default:
+              return Container();
+          }
+        },
       ),
     );
   }
@@ -139,19 +138,26 @@ class _ArticlePreviewWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: SizedBox(
-        height: 150,
         child: Card(
           clipBehavior: Clip.hardEdge,
-          elevation: 10,
-          child: Row(
+          elevation: 0,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: [
-              AspectRatio(
-                aspectRatio: 1,
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  clipBehavior: Clip.hardEdge,
-                  child: Image.network(article.multimedia.first.url),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: AspectRatio(
+                    aspectRatio: 4.0 / 3.0,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      clipBehavior: Clip.hardEdge,
+                      child: Image.network(article.multimedia.first.url),
+                    ),
+                  ),
                 ),
               ),
               Flexible(
@@ -160,52 +166,60 @@ class _ArticlePreviewWidget extends StatelessWidget {
                       const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
                     children: [
+                      Flexible(
+                        child: Opacity(
+                          opacity: .4,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                child: Row(
+                                  children: [
+                                    const Icon(Icons.person, size: 16),
+                                    Flexible(
+                                      child: AutoSizeText(
+                                        article.byline,
+                                        maxFontSize: 12,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Flexible(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    const Icon(Icons.date_range, size: 16),
+                                    Flexible(
+                                      child: AutoSizeText(
+                                        DateFormat('yyyy.MM.dd kk:mm').format(
+                                            DateTime.parse(
+                                                article.createdDate)),
+                                        maxFontSize: 12,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
                       AutoSizeText(
                         article.title,
                         style: GoogleFonts.barlowCondensed(),
-                        minFontSize: 18,
+                        minFontSize: 22,
                         maxLines: 5,
                         overflow: TextOverflow.ellipsis,
                       ),
-                      Flexible(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Flexible(
-                              child: Row(
-                                children: [
-                                  const Icon(Icons.person, size: 16),
-                                  Flexible(
-                                    child: AutoSizeText(
-                                      article.byline,
-                                      maxFontSize: 12,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Flexible(
-                        child: Row(
-                          children: [
-                            const Icon(Icons.date_range, size: 16),
-                            Flexible(
-                              child: AutoSizeText(
-                                DateFormat('yyyy.MM.dd kk:mm').format(
-                                    DateTime.parse(article.createdDate)),
-                                maxFontSize: 12,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
                     ],
                   ),
                 ),
